51453a027ca15bb1b41d96b0726b27fd29ef216e
Angelica Carter <Angelicad87@yahoo.com>
2024-11-14 17:53:05 -0500

    lab5

--- functionDemo.js
+++ functionDemo.js
@@ -9,4 +9,27 @@ function gettingTheSmallerNumber(num1, num2, num3) {
   } else{
     console.log (`${num3} is the smaller num`);
   }
+  }
+// let age = prompt("what is your age?"); 
+function ageChecker (age) {
+
+  if (age >= 18) {
+    display("You are eligible to vote.");
+  }
+  if (age <= 17) {
+    alert("Minors are ineligible to vote");
+  }
+  
+}
+ageChecker (prompt('What is your age?') )
+function determineNumPositiveNegativeZero(num) {
+
+
+  if (num > 0) {
+    return "Positive";
+  } else if (num < 0) {
+    return "negative";
+  } else {
+    return "0";
+  }
 }
